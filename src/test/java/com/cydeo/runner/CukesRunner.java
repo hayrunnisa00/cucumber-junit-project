beba19871b1a7d7cd36a2f90fbf4777108418a2f
Hayrunnisa Apaydin <apaydinhayrunnisa@gmail.com>
2022-11-12 18:48:36 -0500

    CukesRunner.java updated

--- src/test/java/com/cydeo/runner/CukesRunner.java
+++ src/test/java/com/cydeo/runner/CukesRunner.java
@@ -8,6 +8,7 @@ import org.junit.runner.RunWith;
 @RunWith(Cucumber.class)
 @CucumberOptions(
         plugin = {
+                "json:target/cucumber.json",
                 "pretty",
                 "html:target/cucumber-report.html",
                 "rerun:target/rerun.txt",
